21e01df341c1c8cb5bdca3802076324f48ce749b
Administrator <jcsmnd@gmail.com>
2019-04-01 11:40:22 -0400

    get, post, mongoose connection test

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//connect database
+mongoose.connect(config.database);
+mongoose.connection.on('connected', ()=>{
+  console.log('connected to mongoDB'+config.database);
+});
+
+mongoose.connection.on('error', (err)=>{
+  console.log('connection error'+err);
+});
+
+const app = express();
+const port = 3000;
+const users = require('./routes/users');
+
+//cors middleware
+app.use(cors());
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// bodyparser middleware
+app.use(bodyParser.json());
+app.use('/users', users);
+
+// index route
+app.get('/', (req, res) => {
+  res.send('invald endpoint');
+});
+
+// start server
+app.listen(port, () => {
+  console.log('server start'+port);
+});
